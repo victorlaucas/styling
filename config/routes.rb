ebb59182eddada9dd029c056e7ced7d82b124c94
Victor <victorlaucas@gmail.com>
2017-09-26 18:09:12 -0600

    Customized routes for pages and implemented nav bar partial for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :portfolios
   resources :blogs
-  get 'pages/home'
+  root to: "pages#home"
 
-  get 'pages/about'
+  get 'about', to: "pages#about"
 
-  get 'pages/contact'
+  get 'contact', to: "pages#contact"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
